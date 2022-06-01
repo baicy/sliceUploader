39d2a1f1b382fe51c704b95d8b8b4c812099507f
baicy <baicy0118@gmail.com>
2022-06-01 20:34:49 +0800

    feat(core): md5 check

--- src/components/SliceUploader.jsx
+++ src/components/SliceUploader.jsx
@@ -3,10 +3,11 @@ import SparkMD5 from 'spark-md5'
 
 const sliceUploader = () => {
   const [uploadedFileList, setUploadedFileList] = useState([])
-  const [currentFile, setCurrentFile] = useState(null)
+  const [uploadingFile, setUploadingFile] = useState(null)
+  let currentFile = null
   const chunkSize = 5 * 1024 * 1024
   let chunkNum = 1
-  let currentChunk = 0, uplodedChunk = 0
+  let currentChunk = 0
 
   const readFile = (file) => {
     console.group('file', file.name)
@@ -27,54 +28,70 @@ const sliceUploader = () => {
       } else {
         const md5 = spark.end()
         console.log('file read, md5', md5)
+
+        const pos = uploadedFileList.findIndex(v=>v.md5===md5)
+        if(pos>-1) {
+          const list = [...uploadedFileList]
+          list.splice(pos, 1)
+          setUploadedFileList([uploadedFileList[pos], ...list])
+          console.log('file exist')
+        } else {
+          currentFile = { name: file.name, status: false, md5 }
+          setUploadingFile(currentFile)
+          chunkNum>1 ? uploadChunks(file) : uploadFile(file)
+        }
       }
     }
 
     const blobSlice = File.prototype.slice || File.prototype.mozSlice || File.prototype.webkitSlice
 
-    if(chunkNum===1) {
-      fileReader.readAsArrayBuffer(blobSlice.call(file, 0, file.size))
-      uploadFile(file)
-      return
-    }
-
     const readChunk = () => {
       const start = currentChunk * chunkSize, end = (start + chunkSize >= file.size) ? file.size : (start + chunkSize)
       fileReader.readAsArrayBuffer(blobSlice.call(file, start, end))
-      uploadChunk(currentChunk, file)
     }
 
-    setCurrentFile({ name: file.name, status: false })
     readChunk()
   }
 
-  const uploadFile = (file) => {
-    setCurrentFile({ name: file.name, status: false })
+  const uploadFile = () => {
     setTimeout(() => {
       console.log('uploaded file')
       console.groupEnd()
-      setUploadedFileList([{ name: file.name, status: true }, ...uploadedFileList])
-      setCurrentFile(null)
+      currentFile.status = true
+      setUploadedFileList([currentFile, ...uploadedFileList])
+      currentFile = null
+      setUploadingFile(null)
     }, 1000)
   }
 
-  const uploadChunk = (chunk, file) => {
-    setTimeout(() => {
-      console.log('uploaded chunk', chunk)
-      uplodedChunk++
-      if(uplodedChunk===chunkNum) {
-        console.log('uploaded all chunks')
-        mergeChunks(file)
-      }
-    }, 1000)
+  const uploadChunk = (chunk) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log('uploaded chunk', chunk)
+        resolve()
+      }, 1000)
+    })
   }
 
-  const mergeChunks = (file) => {
+  const uploadChunks = (file) => {
+    const reqList = []
+    for(let i = 0; i < chunkNum; i++) {
+      reqList.push(uploadChunk(i))
+    }
+    Promise.all(reqList).then(() => {
+      console.log('uploaded all chunks')
+      mergeChunks()
+    })
+  }
+
+  const mergeChunks = () => {
     setTimeout(() => {
       console.log('merged')
       console.groupEnd()
-      setUploadedFileList([{ name: file.name, status: true }, ...uploadedFileList])
-      setCurrentFile(null)
+      currentFile.status = true
+      setUploadedFileList([currentFile, ...uploadedFileList])
+      currentFile = null
+      setUploadingFile(null)
     }, 1000)
   }
 
@@ -85,12 +102,12 @@ const sliceUploader = () => {
   return <div>
     <input type="file" name="file" id="file" onChange={uploader} />
     <ul>
-      {currentFile && <li>
-        <span>uploading: {currentFile.name} {currentFile.status?.toString()}</span>
+      {uploadingFile && <li>
+        <span>uploading: {uploadingFile.name} {uploadingFile.status?.toString()}</span>
       </li>}
       {
         uploadedFileList.length>0 && uploadedFileList.map((item) => (
-          <li key={item.name}>
+          <li key={item.md5}>
             <span>uploaded: {item.name} {item.status.toString()}</span>
           </li>
         ))
